f30925427007b1168dff9a54eaa5b686ed336473
Olga2001 <57001528+Olga2001@users.noreply.github.com>
2020-12-01 21:23:59 +0300

    Add files via upload

---
+++ src/scripts/model3d.js
@@ -0,0 +1,28 @@
+var loader = new THREE.ObjectLoader();
+
+loader.load(
+	url:"D:\PDXAR:\Sword And Shield Attack.fbx",
+
+	// onLoad callback
+	// Here the loaded data is assumed to be an object
+	function ( obj ) {
+		// Add the loaded object to the scene
+		scene.add( obj );
+	},
+
+	// onProgress callback
+	function ( xhr ) {
+		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+	},
+
+	// onError callback
+	function ( err ) {
+		console.error( 'An error happened' );
+	}
+);
+
+
+// Alternatively, to parse a previously loaded JSON structure
+var object = loader.parse( a_json_object );
+
+scene.add( object );
